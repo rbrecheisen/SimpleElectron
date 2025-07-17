0b0940ab426a22ef284f8417402ac8c6fa4c1104
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-07-17 09:29:11 +0200

    Showing first window

--- app/main.js
+++ app/main.js
@@ -1,6 +1,12 @@
-const {app} = require('electron')
+const {app, BrowserWindow} = require('electron')
 
 
+let main_window = null;
+
+
+// when app is ready, print message to console. This the local terminal, not the webbrowser!
+// Electron apps run locally and do not need a webbrowser.
 app.on('ready', () => {
     console.log('Hello from Electron!');
+    main_window = new BrowserWindow(); // Don't forget "new"!
 });
